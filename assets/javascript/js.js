0206c7029c45b1c71c30deb11d2fe2761009d818
mykolyn <mykolyn@gmail.com>
2020-01-19 20:56:10 -0800

    storing data to firebase

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -3,6 +3,28 @@
 //     alert("clicked")
 // })
 
+
+//firebase
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyCdjP7RtHQZdCEa7enXUGYo4fePqz3qVRE",
+    authDomain: "ucb-test-0108.firebaseapp.com",
+    databaseURL: "https://ucb-test-0108.firebaseio.com",
+    projectId: "ucb-test-0108",
+    storageBucket: "ucb-test-0108.appspot.com",
+    messagingSenderId: "701153986774",
+    appId: "1:701153986774:web:0cf9dcf806202f5a7c2177",
+    measurementId: "G-2N1TNKELQ0"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//firebase ref as var
+var database = firebase.database();
+
+
+
+
 //Initial variables
 var trainName = ""
 var dest = ""
@@ -19,5 +41,26 @@ $("#train-form-submit").on("click", function (){
     var freq = $("#freq").val().trim()
 
     console.log(trainName, dest, firstTrain, freq)
+
+    database.ref("/trainData").push({
+        trainName: trainName,
+        dest: dest,
+        firstTrain: firstTrain,
+        freq: freq
+
+      });
 })
 
+database.ref("/trains").on("child_added", function(snapshot) {
+    snap = snapshot.val();
+
+
+    $("#train-table").find('tbody')
+    .append($('<tr>')
+        .append($('<td>')
+            
+                .text(trainName)
+            )
+        )
+    
+})
